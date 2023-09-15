1eb0668887b5dc2b6e1b367d1f22cf4aba8e7566
Sirius018 <betochirakudai@gmail.com>
2023-09-14 20:10:59 -0500

    Inicio de Tesis

---
+++ src/main/java/com/centroinformacion/controller/TesisRegistroController.java
@@ -0,0 +1,14 @@
+package com.centroinformacion.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+ * @author Juan Ramos
+ *
+ */
+@Controller
+public class TesisRegistroController {
+	     
+	
+}
