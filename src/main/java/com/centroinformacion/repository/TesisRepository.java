ebaad86f6837a6306129e30398bb02a89e111e7a
JuanRamosAnto <juanramosanto@gmail.com>
2023-09-28 21:54:42 -0500

    Avance de Crud Tesis

--- src/main/java/com/centroinformacion/repository/TesisRepository.java
+++ src/main/java/com/centroinformacion/repository/TesisRepository.java
@@ -9,6 +9,11 @@ import com.centroinformacion.entity.Tesis;
 
 public interface TesisRepository extends JpaRepository<Tesis, Integer> {
 
+	
 	@Query("select x from Tesis x where x.titulo = ?1")
 	public  List<Tesis> buscaPorTitulo(String titulo);
+	//1 alude al primer parametro, orden de los parametros  
+	//?2 aludiria al segundo parametro 
+	@Query("select x from Tesis x where x.titulo like ?1")
+	public List<Tesis> listaPorTituloLike(String filtro);
 }
--- src/main/java/com/centroinformacion/service/TesisService.java
+++ src/main/java/com/centroinformacion/service/TesisService.java
@@ -1,6 +1,7 @@
 package com.centroinformacion.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.centroinformacion.entity.Tesis;
 
@@ -8,6 +9,11 @@ public interface TesisService {
 	
 	public abstract Tesis insertaTesis(Tesis obj);
 	public  List<Tesis> buscaPorTitulo(String titulo);
+	//agregamos metodos para el crud 
+	public abstract Tesis actualizaTesis(Tesis obj);
+	public abstract List<Tesis> listaPorTituloLike(String filtro);
+	public abstract Optional<Tesis> buscaTesis(int idTesis);
+	
 	
 
 }
